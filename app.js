1649b4017fb3faf51ad5e11f36b61b2ec8f566a1
ReiTony <reimerstony@gmail.com>
2023-08-28 15:45:42 +0800

    Connected to database and added UserSchema

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
-require('dotenv').config
-require('express-async-errors');
+require('dotenv').config()
+require('express-async-errors')
 
 const express = require('express')
-const app = express
+const app = express()
+const notFoundMiddleware = require('./middleware/not_found')
+const errorHandlerMiddleware = require('./middleware/error_handler')
+//database
+const connectDB = require('./db/connect')
 
 //middleware
 app.use(express.json())
-
-
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 3000
 
+const port = process.env.PORT || 4000
 const start = async () => {
     try {
+        await connectDB(process.env.MONGO_URI)
         app.listen(port, () => 
             console.log(`Server is listening to ${port}....`)
         )
--- middleware/error_handler.js
+++ middleware/error_handler.js
@@ -1,4 +1,4 @@
-const CustomAPIError = require('../errors/custom-error')
+const CustomAPIError = require('../errors/custom_errors')
 const errorHandlerMiddleware = (err, req, res, next) => {
   if (err instanceof CustomAPIError) {
     return res.status(err.statusCode).json({ msg: err.message })
