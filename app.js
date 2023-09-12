6d76ff578030ae16a2b663a751c29532c3586d63
ReiTony <reimerstony@gmail.com>
2023-09-12 11:22:55 +0800

    Edit

--- app.js
+++ app.js
@@ -1,29 +1,45 @@
-require('dotenv').config()
-require('express-async-errors')
+require("dotenv").config();
+require("express-async-errors");
 
-const express = require('express')
-const app = express()
-const notFoundMiddleware = require('./middleware/not_found')
-const errorHandlerMiddleware = require('./middleware/error_handler')
+const express = require("express");
+const app = express();
+//packages
+const cookieParser = require('cookie-parser');
+const helmet = require('helmet');
+const cors = require('cors');
+
 //database
-const connectDB = require('./db/connect')
+const connectDB = require("./db/connect");
+
+//routers
+const loginRouter = require('./routes/loginRoutes');
 
 //middleware
-app.use(express.json())
+const notFoundMiddleware = require("./middleware/not_found");
+const errorHandlerMiddleware = require("./middleware/error_handler");
+
+app.use(cors());
+app.use(helmet());
+
+app.use(express.static('./public'));
+
+app.use(express.json());
+app.use(cookieParser(process.env.JWT_SECRET));
+
+app.use('/api/v1/auth', loginRouter);
+
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 6000;
 const start = async () => {
-    try {
-        await connectDB(process.env.MONGO_URI)
-        app.listen(port, () => 
-            console.log(`Server is listening to ${port}....`)
-        )
-    } catch (error) {
-        console.log(error)
-    }
-}
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => console.log(`Server is listening to ${port}....`));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-start()
+
+start();
