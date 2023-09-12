9de3a9a83e2084da6303f60a514fe093744f19b2
ReiTony <reimerstony@gmail.com>
2023-09-12 11:22:11 +0800

    Error index

---
+++ errors/index.js
@@ -0,0 +1,12 @@
+const CustomAPIError = require('./custom_errors');
+const UnauthenticatedError = require('./unauthenticated');
+const NotFoundError = require('./not_found');
+const BadRequestError = require('./bad_request');
+const UnauthorizedError = require('./unauthorized');
+module.exports = {
+  CustomAPIError,
+  UnauthenticatedError,
+  NotFoundError,
+  BadRequestError,
+  UnauthorizedError,
+};
