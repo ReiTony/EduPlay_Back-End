5661f637b4c8262760dc54bc85bf48b27f23f427
ReiTony <reimerstony@gmail.com>
2023-09-23 00:15:39 +0800

    Refactored Send Email Config

--- utils/sendResetPassword.js
+++ utils/sendResetPassword.js
@@ -1,17 +1,35 @@
-const sendEmail = require('./sendEmail');
+const nodemailer = require("nodemailer");
+require("dotenv").config();
 
-const sendResetPasswordEmail = async ({ name, email, token, origin }) => {
+const createTransporter = async () => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.GMAIL,
+      pass: process.env.PASS,
+    },
+  });
+
+  return transporter;
+};
+
+const sendResetPassword = async ({ name, email, token, origin }) => {
+  const user = getUserByEmail(email);
+
+  const transporter = await createTransporter(user.email, user.password);
+
   const resetURL = `${origin}/user/reset-password?token=${token}&email=${email}`;
-  const message = `<p>Please reset password by clicking on the following link : 
-  <a href="${resetURL}">Reset Password</a></p>`;
+  const message = `<p>Please reset your password by clicking on the following link : 
+    <a href="${resetURL}">Reset Password</a></p>`;
 
-  return sendEmail({
+  return transporter.sendMail({
+    from: '"EduPlay" <eduplay@gmail.com>', 
     to: email,
-    subject: 'Reset Password',
+    subject: "Reset Password",
     html: `<h4>Hello, ${name}</h4>
-   ${message}
-   `,
+      ${message}
+    `,
   });
 };
 
-module.exports = sendResetPasswordEmail;
+module.exports = sendResetPassword;
--- utils/sendVerification.js
+++ utils/sendVerification.js
@@ -1,23 +1,32 @@
-const sendEmail = require('./sendEmail');
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+const createTransporter = async () => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.GMAIL,
+      pass: process.env.PASS,
+    },
+  });
 
-const sendVerificationEmail = async ({
-  name,
-  email,
-  verificationToken,
-  origin,
-}) => {
+  return transporter;
+};
+
+const sendVerification = async ({ name, email, verificationToken, origin }) => {
+  const transporter = await createTransporter();
+
   const verifyEmail = `${origin}/user/verify-email?token=${verificationToken}&email=${email}`;
-
   const message = `<p>Please confirm your email by clicking on the following link : 
-  <a href="${verifyEmail}">Verify Email</a> </p>`;
+    <a href="${verifyEmail}">Verify Email</a> </p>`;
 
-  return sendEmail({
+  return transporter.sendMail({
+    from: '"EduPlay" <eduplay1909@gmail.com>', 
     to: email,
-    subject: 'Email Confirmation',
-    html: `<h4> Hello, ${name}</h4>
-    ${message}
+    subject: "Email Confirmation",
+    html: `<h4>Hello, ${name}</h4>
+      ${message}
     `,
   });
 };
 
-module.exports = sendVerificationEmail;
+module.exports = sendVerification;
