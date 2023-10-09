842712c8e98f1d04d1e61a1d7417328f65530850
ReiTony <reimerstony@gmail.com>
2023-10-09 11:14:59 +0800

    Refactor

--- routes/TeacherRoutes.js
+++ routes/TeacherRoutes.js
@@ -10,6 +10,7 @@ const {
   getSingleStudent,
   updateStudent,
   deleteStudent,
+  getAllStudents
 } = require("../controllers/studentController");
 const {
   currentTeacher,
@@ -36,7 +37,7 @@ router.get(
   "/Class",
   //authenticateUser,
   //authorizePermissions("teacher"),
-  getClass
+  getAllStudents
 );
 router.post("/addStudent", studentRegister);
 router.get("/showStudent/:id",  getSingleStudent);
