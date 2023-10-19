3f28e66f558a45dd880ea0cbb50041081562ea28
ReiTony <reimerstony@gmail.com>
2023-10-19 18:41:44 +0800

    Refactored CheckPerms Util

--- utils/checkPerms.js
+++ utils/checkPerms.js
@@ -1,7 +1,7 @@
 const CustomError = require('../errors');
 
 const checkPermissions = (requestUser, resourceUserId) => {
-  if (requestUser.role === 'Admin' || requestUser.role === 'Teacher') return;
+  if (requestUser.role === 'Admin' || requestUser.role === 'Teacher' || requestUser.role === 'Student') return;
   if (requestUser.userId === resourceUserId.toString()) return;
   throw new CustomError.UnauthorizedError(
     'Not authorized to access this route'
