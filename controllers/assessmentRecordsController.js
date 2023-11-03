517b2c7a21d2fb1e6e0f25b35fb003365383a9e2
j-sabile <jericosabile@gmail.com>
2023-11-03 11:46:42 +0800

    prevent students from taking an assessment twice

--- controllers/assessmentRecordsController.js
+++ controllers/assessmentRecordsController.js
@@ -9,10 +9,11 @@ const createAssessmentRecord = async (req, res) => {
     if (!moduleNumber || !userId || !answers) return res.status(400).json({ message: "moduleNumber, userId, and answers are required." });
     const student = await Student.findById(userId);
     if (!student) return res.status(404).json({ message: "Student not found" });
+    if ((await AssessmentRecord.find({ studentId: userId, gradeLevel: student.gradeLevel, moduleNumber })).length > 0) return res.status(400).json({ message: "You have already taken this assessment!" });
     const module = getModuleSync(moduleNumber, student.gradeLevel, "assessment");
     if (module.questions.length !== answers.length) return res.status(400).json({ message: `The number of questions (${module.questions.length}) and answer (${answers.length}) does not match.` });
     let score = 0;
-    const badges = module.categories.map((i) => ({category: i, score: 0, total: 0}));
+    const badges = module.categories.map((i) => ({ category: i, score: 0, total: 0 }));
     for (let i = 0; i < module.questions.length; i++) {
       if (module.questions[i].correctAnswer === answers[i]) {
         score++;
@@ -23,7 +24,7 @@ const createAssessmentRecord = async (req, res) => {
     await saveBadges(userId, badges);
     const newAssessmentRecord = new AssessmentRecord({ studentId: userId, score: score, moduleNumber, gradeLevel: student.gradeLevel, answers });
     await newAssessmentRecord.save();
-    res.status(200).json({ message: "Success" });
+    res.status(200).json({ message: "Success", score });
   } catch (error) {
     console.log(error.message);
     res.status(500).json({ message: error.message });
