29303e0259f0811e62c458baa4a0a0f4923be960
ReiTony <reimerstony@gmail.com>
2023-11-04 04:00:25 +0800

    Refactor notificationSchema.js to include recipient field

--- models/notificationSchema.js
+++ models/notificationSchema.js
@@ -1,19 +1,29 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const NotificationSchema = new mongoose.Schema({
   message: {
     type: String,
     required: true,
   },
+  recipient: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Student', 
+  },
   assessment: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: "Assessment",
+    ref: 'Assessment',
   },
   gradeLevel: {
     type: Number,
     enum: [1, 2, 3],
     required: true,
   },
-}, { timestamps: true });
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-module.exports = mongoose.model("Notification", NotificationSchema);
+const Notification = mongoose.model('Notification', NotificationSchema);
+
+module.exports = Notification;
