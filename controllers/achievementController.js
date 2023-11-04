0fefbe2a6d2166f06fa252ab2733aeb3e0724395
ReiTony <reimerstony@gmail.com>
2023-11-05 03:04:06 +0800

    Refactor achievementController.js to limit achievements to 10 and sort by createdAt.

--- controllers/achievementController.js
+++ controllers/achievementController.js
@@ -5,15 +5,22 @@ const CustomError = require("../errors");
 const getAchievements = async (req, res, next) => {
   try {
     const studentId = req.params.userId;
-    const achievements = await Achievement.findOne(studentId).lean();
-    if (!achievements) {
+    const achievements = await Achievement.findOne({ studentId })
+      .sort({ createdAt: -1 })
+      .limit(10);
+
+    console.log(`Achievements:`, achievements);
+
+    if (achievements.length === 0) {
       throw new CustomError.NotFoundError("Achievements not found");
     }
+
     res.status(StatusCodes.OK).json(achievements);
   } catch (error) {
     next(error);
   }
 };
+
 module.exports = {
   getAchievements,
 };
