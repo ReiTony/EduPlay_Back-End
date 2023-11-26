85ca3c0f19299b63ef4a873039589037291a634c
ReiTony <reimerstony@gmail.com>
2023-11-26 18:33:33 +0800

    Remove commented out code for forgot password functionality

--- routes/TeacherRoutes.js
+++ routes/TeacherRoutes.js
@@ -41,7 +41,6 @@ router.post("/login", teacherLogin);
 router.delete("/logout", authenticateUser, teacherLogout);
 router.post("/verify-email", teacherVerifyEmail);
 router.post("/reset-password", teacherResetPassword);
-// router.post("/forgot-password", teacherForgotPassword);
 
 // Teacher Manages Student Account with same GradeLevel
 router.get("/Class", getAllStudents);
