719c8e51492880c60375da05cf575f58f80b35bd
ReiTony <reimerstony@gmail.com>
2023-11-27 17:34:34 +0800

    Refactor teacherController.js

--- controllers/teacherController.js
+++ controllers/teacherController.js
@@ -6,8 +6,6 @@ const {
   attachCookiesToResponse,
   createTokenUser,
   sendVerification,
-  sendResetPassword,
-  createHash,
 } = require("../utils");
 const crypto = require("crypto");
 
@@ -222,7 +220,6 @@ const teacherLogout = async (req, res, next) => {
 //   }
 // };
 
-
 const teacherResetPassword = async (req, res) => {
   try {
     const { email } = req.body;
@@ -237,7 +234,7 @@ const teacherResetPassword = async (req, res) => {
       throw new CustomError.NotFoundError("Teacher not found");
     }
 
-    const newPassword = req.body.newPassword; 
+    const newPassword = req.body.newPassword;
     if (!newPassword) {
       throw new CustomError.BadRequestError("Please provide the new password");
     }
@@ -245,7 +242,6 @@ const teacherResetPassword = async (req, res) => {
     await teacher.save();
 
     res.send("Password Reset Successfully");
-
   } catch (error) {
     console.error("Error in teacherResetPassword:", error);
 
@@ -282,12 +278,143 @@ const teacherResetPassword = async (req, res) => {
 
 //   res.send("Password Reset Successfully");
 // };
+
+// Manage Teacher For Admin
+
+const getAllTeachers = async (req, res) => {
+  try {
+    const teachers = await Teacher.find({ role: "Teacher" });
+    res.status(StatusCodes.OK).json({ teachers });
+  } catch (error) {
+    console.error(error);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      error: "An error occurred while processing your request.",
+    });
+  }
+};
+
+const getSingleTeacher = async (req, res) => {
+  try {
+    const teacher = await Teacher.findOne({ email: req.params.id });
+    if (!teacher) {
+      throw new CustomError.NotFoundError(
+        `No teacher with id : ${req.params.id}`
+      );
+    }
+    res.status(StatusCodes.OK).json({ teacher });
+  } catch (error) {
+    console.error(error);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      error: "An error occurred while processing your request.",
+    });
+  }
+};
+
+const updateTeacher = async (req, res) => {
+  try {
+    const { email, username, name } = req.body;
+    if (!email || !username) {
+      throw new CustomError.BadRequestError("Please provide all values");
+    }
+    const teacher = await Teacher.findOne({ _id: req.teacher.teacherId });
+
+    teacher.email = email;
+    teacher.username = username;
+    teacher.name = name;
+
+    await teacher.save();
+
+    const tokenTeacher = createTokenUser(teacher);
+    attachCookiesToResponse({ res, user: tokenTeacher });
+    res.status(StatusCodes.OK).json({ teacher: tokenTeacher });
+  } catch (error) {
+    if (error instanceof CustomError.BadRequestError) {
+      res.status(StatusCodes.BAD_REQUEST).json({ error: error.message });
+    } else if (error instanceof CustomError.NotFoundError) {
+      res.status(StatusCodes.NOT_FOUND).json({ error: error.message });
+    } else {
+      console.error("An error occurred:", error);
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+        error: "Internal server error",
+      });
+    }
+  }
+};
+
+const updateTeacherPassword = async (req, res) => {
+  try {
+    const { oldPassword, newPassword } = req.body;
+    if (!oldPassword || !newPassword) {
+      throw new CustomError.BadRequestError("Please provide both values");
+    }
+    const teacher = await Teacher.findOne({ _id: req.teacher.teacherId });
+
+    const isPasswordCorrect = await teacher.comparePassword(oldPassword);
+    if (!isPasswordCorrect) {
+      throw new CustomError.UnauthenticatedError("Invalid Credentials");
+    }
+    teacher.password = newPassword;
+
+    await teacher.save();
+    res.status(StatusCodes.OK).json({ msg: "Success! Password Updated." });
+  } catch (error) {
+    if (error instanceof CustomError.BadRequestError) {
+      res.status(StatusCodes.BAD_REQUEST).json({ error: error.message });
+    } else if (error instanceof CustomError.UnauthenticatedError) {
+      res.status(StatusCodes.UNAUTHORIZED).json({ error: error.message });
+    } else {
+      console.error("An error occurred:", error);
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+        error: "Internal server error",
+      });
+    }
+  }
+};
+
+const deleteTeacher = async (req, res) => {
+  try {
+    const teacherId = req.params.teacherId;
+
+    if (!teacherId) {
+      throw new CustomError.BadRequestError(
+        "Please provide a valid teacher ID"
+      );
+    }
+    const teacher = await Teacher.findOne({ _id: teacherId });
+
+    if (!teacher) {
+      throw new CustomError.NotFoundError("Teacher not found");
+    }
+
+    await teacher.remove();
+
+    res
+      .status(StatusCodes.OK)
+      .json({ message: "Teacher deleted successfully" });
+  } catch (error) {
+    if (error instanceof CustomError.BadRequestError) {
+      res.status(StatusCodes.BAD_REQUEST).json({ error: error.message });
+    } else if (error instanceof CustomError.NotFoundError) {
+      res.status(StatusCodes.NOT_FOUND).json({ error: error.message });
+    } else {
+      console.error("An error occurred:", error);
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+        error: "Internal server error",
+      });
+    }
+  }
+};
+
 module.exports = {
   teacherRegister,
   teacherLogin,
   teacherLogout,
   teacherVerifyEmail,
-  // teacherForgotPassword,
   teacherResetPassword,
   currentTeacher,
+  getAllTeachers,
+  getSingleTeacher,
+  updateTeacher,
+  updateTeacherPassword,
+  deleteTeacher,
 };
