6b4bd3bf812c6a8b4bf05b0bf6ec78cf60495332
ReiTony <reimerstony@gmail.com>
2023-11-28 17:23:45 +0800

    Add teacher management routes

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -22,12 +22,30 @@ const {
   getSingleModule,
   getModulesByGradeLevel,
 } = require("../controllers/fileController");
+
+const {
+  getAllTeachers,
+  getSingleTeacher,
+  updateTeacher,
+  updateTeacherPassword,
+  deleteTeacher,
+  teacherRegister,
+} = require("../controllers/teacherController");
+
 // Admin
 router.get("/", currentAdmin);
 router.post("/register", adminRegister);
 router.post("/login", adminLogin);
 router.delete("/logout", adminLogout);
 router.post("/reset-password", adminResetPassword);
+
+// Admin Manages Teachers
+router.get("/teachers", getAllTeachers);
+router.post("/addTeacher", teacherRegister);
+router.get("/showTeacher/:id", getSingleTeacher);
+router.patch("/updateTeacher/:id", updateTeacher);  
+router.patch("/updateTeacherPassword/:id", updateTeacherPassword);
+router.delete("/deleteTeacher/:id", deleteTeacher);
 
 // Admin Manages Students
 router.get("/students", getAllStudents);
