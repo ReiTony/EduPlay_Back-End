d4ff568c8ca78e2fdff167efa11b4c5f2ab2bcea
ReiTony <reimerstony@gmail.com>
2023-12-06 21:58:19 +0800

    Add email template for verification

--- utils/sendVerification.js
+++ utils/sendVerification.js
@@ -1,5 +1,7 @@
 const nodemailer = require("nodemailer");
+const Mailgen = require("mailgen");
 require("dotenv").config();
+
 const createTransporter = async () => {
   const transporter = nodemailer.createTransport({
     service: "gmail",
@@ -15,17 +17,41 @@ const createTransporter = async () => {
 const sendVerification = async ({ name, email, verificationToken, origin }) => {
   const transporter = await createTransporter();
 
+  const mailGenerator = new Mailgen({
+    theme: "salted",
+    product: {
+      name: "EduPlay",
+      link: "https://eduplay-lhjs.onrender.com/",
+      logo: "https://your-logo-url.com/logo.png",
+    },
+  });
+
   const verifyEmail = `${origin}/user/verify-email?token=${verificationToken}&email=${email}`;
-  const message = `<p>Please confirm your email by clicking on the following link : 
-    <a href="${verifyEmail}">Verify Email</a> </p>`;
+
+  const emailContent = {
+    body: {
+      name: "EduPlay",
+      title: "Email Confirmation",
+      intro: "Please confirm your email by clicking on the following link:",
+      action: {
+        instructions:
+          "Verifying your email gives you access to EduPlay. Click the button below:",
+        button: {
+          color: "#22BC66",
+          text: "CONFIRM EMAIL",
+          link: verifyEmail,
+        },
+      },
+    },
+  };
+
+  const emailTemplate = mailGenerator.generate(emailContent);
 
   return transporter.sendMail({
-    from: '"EduPlay" <eduplay1909@gmail.com>', 
+    from: '"EduPlay" <eduplay1909@gmail.com>',
     to: email,
     subject: "Email Confirmation",
-    html: `<h4>Hello, ${name}</h4>
-      ${message}
-    `,
+    html: emailTemplate,
   });
 };
 
